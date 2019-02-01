349dde195ec56d08dab20dbd931638c9655a113c
Andrea Malara <andrea.malara@cern.ch>
2019-02-01 15:56:09 +0100

    update

--- code/maxwell_job.sh
+++ code/maxwell_job.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-#SBATCH --partition=maxwell
-#SBATCH --time=10:00:00                 # Maximum time request
+#SBATCH --partition=all
+#SBATCH --time=5:00:00                  # Maximum time request
 #SBATCH --nodes=1                       # Number of nodes
-#SBATCH --workdir   /beegfs/desy/user/amalara/code/
-#SBATCH --job-name  JetImage
-#SBATCH --output    log_JetImage/sbatch-%N-%j.out  # File to which STDOUT will be written
-#SBATCH --error     log_JetImage/sbatch-%N-%j.err  # File to which STDERR will be written
+#SBATCH --constraint=GPU
+#SBATCH --workdir   /beegfs/desy/user/amalara/output_varariables/workdir
+#SBATCH --job-name  Sequential
+#SBATCH --output    log_Sequential/sbatch-%N-%j.out  # File to which STDOUT will be written
+#SBATCH --error     log_Sequential/sbatch-%N-%j.err  # File to which STDERR will be written
 #SBATCH --mail-type ALL                 # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user andrea.malara@desy.de  # Email to which notifications will be sent
 
 
-python JetImage.py
+source ~/.setenv
+
+cd /beegfs/desy/user/amalara/DeepWWTagger/code
+python Sequential.py
